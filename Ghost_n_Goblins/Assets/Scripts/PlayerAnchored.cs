ff05be7ab984bc7a54116db66a59bb23c825c06f
Martin Berthier <martinberthier@free.fr>
2021-10-28 13:43:15 +0200

    d

--- Ghost_n_Goblins/Assets/Scripts/PlayerAnchored.cs
+++ Ghost_n_Goblins/Assets/Scripts/PlayerAnchored.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour {
+public class PlayerAnchored : MonoBehaviour {
     [SerializeField] GameObject _Player;
     [SerializeField] float _yOffset;
 
     void Update() {
-        transform.position = new Vector3(_Player.transform.position.x, _Player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(_Player.transform.position.x, _Player.transform.position.y + _yOffset, transform.position.z);
     }
 }
