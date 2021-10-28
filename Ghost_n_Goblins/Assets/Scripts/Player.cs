1cb71f83ef860ab96f8033d13580a0c39933102e
Martin Berthier <martinberthier@free.fr>
2021-10-28 14:37:31 +0200

    [add] Background w/ parallax

--- Ghost_n_Goblins/Assets/Scripts/Player.cs
+++ Ghost_n_Goblins/Assets/Scripts/Player.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,74 +42,3 @@ public class Player : MonoBehaviour
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")) _IsGrounded = false;
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour
-{
-    public enum State { IDLE, JUMP }; // States of character
-    public State m_state = State.IDLE;
-    
-    [SerializeField]
-    float playerSpeed = 10f;
-    [SerializeField]
-    float jumpForce = 10f;
-
-    private bool isJump = false;
-    Rigidbody2D m_rigidbody2D;
-
-   
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float xInput = Input.GetAxis("Horizontal") * playerSpeed;
-
-        //if (m_state == State.IDLE)
-        {
-            // Movement
-            Vector3 movement = xInput * transform.right * playerSpeed * Time.deltaTime;
-            transform.Translate(movement);
-            //m_rigidbody2D.MovePosition(m_rigidbody2D.position + (Vector2)movement);
-
-            // Jump
-            if (Input.GetKeyDown(KeyCode.Space))
-                isJump = true;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        // Jump
-        if (isJump)
-        {
-            m_rigidbody2D.velocity = Vector3.up * jumpForce;
-           // m_rigidbody2D.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse); 
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))       
-            m_state = State.IDLE;
-        
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            m_state = State.JUMP;
-            isJump = false;
-        }
-        
-    }
-}
->>>>>>> Stashed changes
--- Ghost_n_Goblins/Assets/Scripts/ScrollingBackground.cs
+++ Ghost_n_Goblins/Assets/Scripts/ScrollingBackground.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class ScrollingBackground : MonoBehaviour {
     [SerializeField] GameObject _Background;
-    [SerializeField] float _Choke;
+
     Camera _MainCamera;
     Vector2 _ScreenBounds;
 
     private void Start() {
         _MainCamera = gameObject.GetComponent<Camera>();
         _ScreenBounds = _MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, _MainCamera.transform.position.z));
-        float BackgroundWidth = _Background.GetComponent<SpriteRenderer>().bounds.size.x + _Choke;
-        int childsNeeded = (int)Mathf.Ceil(_ScreenBounds.x * 2 / BackgroundWidth);
+        InitializeChildren();
+    }
+
+    void InitializeChildren() {
+        float BackgroundWidth = _Background.GetComponent<SpriteRenderer>().bounds.size.x;
+        int childsNeeded = 2;
         GameObject clone = Instantiate(_Background) as GameObject;
-        for(int i = 0; i <= childsNeeded; i++) {
+        for (int i = 0; i <= childsNeeded; i++) {
             GameObject c = Instantiate(clone) as GameObject;
             c.transform.SetParent(_Background.transform);
             c.transform.position = new Vector3(BackgroundWidth * i, _Background.transform.position.y, _Background.transform.position.z);
@@ -28,12 +32,12 @@ public class ScrollingBackground : MonoBehaviour {
         if(children.Length > 1) {
             GameObject firstChild = children[1].gameObject;
             GameObject lastChild = children[children.Length - 1].gameObject;
-            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x - choke;
-            if(transform.position.x + _ScreenBounds.x > lastChild.transform.position.x + halfObjectWidth) {
+            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+            if(_MainCamera.transform.position.x > lastChild.transform.position.x + (halfObjectWidth/4)) {
                 firstChild.transform.SetAsLastSibling();
                 firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2, lastChild.transform.position.y, lastChild.transform.position.z);
             }
-            else if(transform.position.x - _ScreenBounds.x < firstChild.transform.position.x - halfObjectWidth) {
+            else if(_MainCamera.transform.position.x < firstChild.transform.position.x + (halfObjectWidth/4)) {
                 lastChild.transform.SetAsFirstSibling();
                 lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth * 2, firstChild.transform.position.y, firstChild.transform.position.z);
             }
