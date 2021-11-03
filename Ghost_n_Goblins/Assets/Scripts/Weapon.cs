937daef22bfb16887fc127730dc3af278a138b50
Bryan <blueshark936@gmail.com>
2021-11-03 13:39:20 +0100

    [WIP] Shoot

--- Ghost_n_Goblins/Assets/Scripts/Weapon.cs
+++ Ghost_n_Goblins/Assets/Scripts/Weapon.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField]
+    float m_speed = 10f;
+
+    Rigidbody2D rb2D;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D.AddForce(transform.right * m_speed, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject, 3f);
     }
 }
