347e246b7cf6948ea2541bebd184ee30ff0e0ba2
Martin Berthier <martinberthier@free.fr>
2021-11-10 13:32:39 +0100

    [Add] Dynamic UI For Weapons

--- Ghost_n_Goblins/Assets/Scripts/EventManagers/HudManager.cs
+++ Ghost_n_Goblins/Assets/Scripts/EventManagers/HudManager.cs
@@ -8,7 +8,7 @@
     public class HudManager : Manager<HudManager> {
         #region Variables
         [Tooltip("HUD GameObject (global parent)")]
-        [SerializeField] GameObject HUD;
+        [SerializeField] GameObject _HUD;
 
         [Tooltip("Score textfield")]
         [SerializeField] Text _Score;
@@ -17,16 +17,33 @@
         [SerializeField] Text _Timer;
 
         [Tooltip("Life UI Image prefab")]
-        [SerializeField] GameObject LifePrefab;
+        [SerializeField] GameObject _LifePrefab;
+
+        [Tooltip("Lance UI Image")]
+        [SerializeField] GameObject _LanceUI;
+
+        [Tooltip("Dagger UI Image")]
+        [SerializeField] GameObject _DaggerUI;
+
+        [Tooltip("Torch UI Image")]
+        [SerializeField] GameObject _TorchUI;
+
+        [Tooltip("Axe UI Image")]
+        [SerializeField] GameObject _AxeUI;
+
+        [Tooltip("Shield UI Image")]
+        [SerializeField] GameObject _ShieldUI;
 
         // List of gameobject who represents lives in the HUD
-        List<GameObject> Lives = new List<GameObject>();
+        List<GameObject> _Lives = new List<GameObject>();
+        List<GameObject> _WeaponUiImages = new List<GameObject>();
         #endregion
 
         #region Manager implementation
         protected override void Awake() {
             base.Awake();
             SetHudActive(false);
+            RegisterWeapons();
         }
 
         protected override IEnumerator InitCoroutine() {
@@ -34,7 +51,52 @@
         }
         #endregion
 
+        public override void SubscribeEvents() {
+            base.SubscribeEvents();
+            EventManager.Instance.AddListener<WeaponSwapEvent>(WeaponSwap);
+        }
+
+        public override void UnsubscribeEvents() {
+            base.UnsubscribeEvents();
+            EventManager.Instance.RemoveListener<WeaponSwapEvent>(WeaponSwap);
+        }
+
         #region HudManager Methods
+        void ActivateWeapon(GameObject weapon) {
+            // activate the specific weapon
+            foreach (var item in _WeaponUiImages)
+                if (item) item.SetActive(item == weapon);
+        }
+
+        void RegisterWeapons() {
+            _WeaponUiImages.Add(_LanceUI);
+            _WeaponUiImages.Add(_DaggerUI);
+            _WeaponUiImages.Add(_TorchUI);
+            _WeaponUiImages.Add(_AxeUI);
+            _WeaponUiImages.Add(_ShieldUI);
+        }
+        void ChangeUiWeapon(Weapon weapon) {
+            switch (weapon) {
+                case Weapon.Lance:
+                    ActivateWeapon(_LanceUI);
+                    break;
+                case Weapon.Dagger:
+                    ActivateWeapon(_DaggerUI);
+                    break;
+                case Weapon.Torch:
+                    ActivateWeapon(_TorchUI);
+                    break;
+                case Weapon.Axe:
+                    ActivateWeapon(_AxeUI);
+                    break;
+                case Weapon.Shield:
+                    ActivateWeapon(_ShieldUI);
+                    break;
+                default:
+                    break;
+            }
+        }
+
         void SetScoreAndTimerUI(int score, float timer) {
             // Set the score and the timer
             _Score.text = score.ToString();
@@ -43,27 +105,27 @@
 
         void SetHudActive(bool active) {
             // Disable or Enable the HUD component
-            HUD.SetActive(active);
+            _HUD.SetActive(active);
         }
 
         void SetLivesUI(int NLives) { //bug -> pas le bon scaling
             // Change the number of lives displayed on the UI
-            int actualLives = Lives.Count;
-            float imageWidth = LifePrefab.transform.GetComponent<Image>().preferredWidth * LifePrefab.transform.localScale.x;
+            int actualLives = _Lives.Count;
+            float imageWidth = _LifePrefab.transform.GetComponent<Image>().preferredWidth * _LifePrefab.transform.localScale.x;
             if (NLives == actualLives) return;
 
             else if(NLives > actualLives) {
-                for (int i = Lives.Count; i < NLives; i++) {
-                    GameObject live = Instantiate(LifePrefab) as GameObject;
+                for (int i = _Lives.Count; i < NLives; i++) {
+                    GameObject live = Instantiate(_LifePrefab) as GameObject;
                     live.transform.position = new Vector3(live.transform.position.x + i * imageWidth, live.transform.position.y, live.transform.position.y);
-                    live.transform.SetParent(HUD.transform);
-                    Lives.Add(live);
+                    live.transform.SetParent(_HUD.transform);
+                    _Lives.Add(live);
                 }
             }
 
             else if(NLives < actualLives) {
                 for(int i = actualLives; i > NLives - actualLives; i--) {
-                    Lives.RemoveAt(i);
+                    _Lives.RemoveAt(i);
                 }
             }
         }
@@ -78,6 +140,10 @@
             SetScoreAndTimerUI(e.eScore, e.eTimer);
             SetLivesUI(e.eNLives);
         }
+
+         void WeaponSwap(WeaponSwapEvent e) {
+            ChangeUiWeapon(e.eWeapon);
+         }
         #endregion
     }
 }
